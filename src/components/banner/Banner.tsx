3058ea0b5508b36278f63b0abf0ab81e845202ad
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2023-05-23 17:28:54 -0500

    optimized code

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -14,6 +14,7 @@ export default function Banner({ id, url }: Props) {
           alt={`${url}`}
           width={1440}
           height={630}
+          priority
         />
         <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90 ">
           COMPRAR
