fb88114caf19b213fabac3dd13a60754bfb4b608
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2023-06-01 11:11:33 -0500

    Image priority true

--- src/components/product/Carousel.tsx
+++ src/components/product/Carousel.tsx
@@ -21,6 +21,7 @@ const ProductDetailsCarousel: FC<Props> = ({ data }) => {
       <Carousel
         showIndicators={false}
         showStatus={false}
+        animationHandler={"fade"}
         thumbWidth={50}
         className="productCarousel"
         renderThumbs={() =>
@@ -31,19 +32,20 @@ const ProductDetailsCarousel: FC<Props> = ({ data }) => {
                 width={50}
                 height={60}
                 alt={img.attributes.name}
+                priority={false}
               />
             </div>
           ))
         }
       >
         {data.map((img, idx) => (
-          <div className="max-w-[520px] max-h-[650px]" key={idx}>
+          <div className="w-full h-fulls max-w-[520px] max-h-[650px] bg-white" key={idx} >
             <Image
               src={img.attributes.url}
               alt={`${img.attributes.name} preview ${idx}`}
               width={520}
               height={650}
-              priority={idx === 0}
+              priority={true}
             />
           </div>
         ))}
