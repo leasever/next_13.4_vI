e169e293a9f3bf8d8345c9e02c795d9235cbc6da
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2023-06-01 15:57:21 -0500

    aspec ratio banner

--- src/components/category/CategoryCard.tsx
+++ src/components/category/CategoryCard.tsx
@@ -7,17 +7,16 @@ function CategoryCard({ attributes }: Category) {
     <>
       <div className="group relative">
         <div className="relative h-96 w-full overflow-hidden rounded-lg bg-white  group-hover:opacity-75 ">
-            <Image
-              width={630}
-              height={400}
-              src={attributes.image.data[0].attributes.url}
-              alt={attributes.name}
-              className=" h-full w-full object-cover object-center"
-            />
+          <Image
+            width={630}
+            height={400}
+            src={attributes.image.data[0].attributes.url}
+            alt={attributes.name}
+            priority={true}
+          />
         </div>
         <h3 className="mt-6 text-lg text-gray-500">
           <Link href={`/catalogue/category/${attributes.slug}/1`}>
-            <span className="absolute inset-0"></span>
             {attributes.name.toUpperCase()}
           </Link>
         </h3>
