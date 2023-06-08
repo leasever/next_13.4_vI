e9a073960597a222d46c03c35c173b408db3d63d
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2023-06-08 01:20:04 -0500

    optimized code

--- src/components/cart/PaymenButton.tsx
+++ src/components/cart/PaymenButton.tsx
@@ -1,30 +1,14 @@
+import { payment } from "@/app/catalogue/services";
 import { CartInterface } from "@/models/cart.model";
-import { makePaymentRequest } from "@/utils/api";
-import { STRIPE_API_TOKEN } from "@/utils/urls";
-import { loadStripe } from "@stripe/stripe-js";
 import { useState } from "react";
-
-const stripePromise = loadStripe(STRIPE_API_TOKEN as string);
 
 const PaymentButton = ({ items }: CartInterface) => {
   const [loading, setLoading] = useState(false);
 
   const handlePayment = async () => {
     setLoading(true);
-    try {
-      const stripe = await stripePromise;
-      const res = await makePaymentRequest("/api/orders", {
-        products: items,
-      });
-
-      await stripe?.redirectToCheckout({
-        sessionId: res.stripeSession.id,
-      });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
+    await payment({ items });
+    setLoading(false);
   };
 
   return (
