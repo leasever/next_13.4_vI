15ff2a7f9f69f295d2d2b555a4bbd654efe42b54
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2023-06-09 08:46:53 -0500

    client priority false imageLoader in banner

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 import Image, { ImageLoaderProps } from "next/image";
 import { FC } from "react";
 
@@ -8,17 +8,16 @@ interface BannerProps {
 
 export const Banner: FC<BannerProps> = ({ url }) => {
   const imageLoader = ({ src, width, quality }: ImageLoaderProps): string => {
-    return `${url}?w=${1440}&q=${500}`;
+    return `${src}?w=${width}&q=${quality || 500}`;
   };
   return (
     <div className="relative w-full max-h-[630px]">
       <Image
-        width={1440}
-        height={630}
         loader={imageLoader}
         src={`/${url}`}
+        width={1440}
+        height={630}
         alt={url}
-        priority={true}
       />
     </div>
   );
