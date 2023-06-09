b6f80c2f38806cbcf0431d077725cff55625d3a9
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2023-06-09 07:41:25 -0500

    image banner loader

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -5,15 +5,20 @@ interface BannerProps {
   url: string;
 }
 
-export const Banner: FC<BannerProps> = ({ url }) => (
-  <div className="relative w-full max-h-[630px]">
-    <Image
-      src={`/${url}`}
-      alt={url}
-      width={1440}
-      height={630}
-      quality={100}
-      priority={true}
-    />
-  </div>
-);
+export const Banner: FC<BannerProps> = ({ url }) => {
+  const imageLoader = ({ src, width, quality }: any) => {
+    return `${src}?w=${width}&q=${quality || 100}`;
+  };
+  return (
+    <div className="relative w-full max-h-[630px]">
+      <Image
+        src={`/${url}`}
+        alt={url}
+        width={1440}
+        height={630}
+        priority={true}
+        loader={imageLoader}
+      />
+    </div>
+  );
+};
