5822cfd07b7ac470bc9a47dd847a499399e8e155
leasever <leandrovegaabraham@gmail.com>
2023-07-13 09:42:01 -0500

    optimized code 1.2

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,8 +1,11 @@
 export const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 export const STRIPE_API_TOKEN = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
 
-export const API_URL = process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://127.0.0.1:1337";
+export const API_URL =
+  process.env.NEXT_PUBLIC_STRAPI_API_URL ||
+  "https://quotation-api-serve.onrender.com";
 
-export const CLIENT_URL = process.env.NEXT_PUBLIC_CLIENT_URL || 'http://localhost:3000';
+export const CLIENT_URL =
+  process.env.NEXT_PUBLIC_CLIENT_URL || "http://localhost:3000";
 
 export const OPENIA_URL = process.env.NEXT_PUBLIC_OPENIA_URL;
