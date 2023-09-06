ad574a9ef4ccc0b517d085eabdaf994a8a015b13
leasever <leandrovegaabraham@gmail.com>
2023-09-06 12:40:09 -0500

    proyect m3-4

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -3,6 +3,8 @@ export const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 export const API_URL =
   process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://127.0.0.1:1337";
 
-export const CLIENT_URL = "https://consorcio-ac-electrica-sac.vercel.app";
+export const CLIENT_URL =
+  process.env.NEXT_PUBLIC_CLIENT_API_URL ||
+  "https://consorcio-ac-electrica-sac.vercel.app";
 
 export const OPENIA_URL = process.env.NEXT_PUBLIC_OPENIA_URL;
